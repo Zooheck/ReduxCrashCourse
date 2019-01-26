53d189c9066d1c0ce3137e73566ca7851a8ecb5e
Zooheck <davidscottflack@gmail.com>
2019-01-26 12:37:42 -0600

    Add actions and reducers

--- redux/src/App.js
+++ redux/src/App.js
@@ -2,13 +2,13 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { Provider } from 'react-redux'
-import { createStore, applyMiddleware } from 'redux'
 
+import store from './store'
 
 import Posts from './components/Posts'
 import PostForm from './components/PostForm'
 
-const store = createStore(() => [], {}, applyMiddleware())
+
 
 class App extends Component {
   render() {
