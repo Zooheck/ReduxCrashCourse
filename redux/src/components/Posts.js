e574cc21b8d79be1ea0bf30c24e5292c5da5fc4e
Zooheck <davidscottflack@gmail.com>
2019-01-26 12:42:29 -0600

    Add post to beginning of list

--- redux/src/components/Posts.js
+++ redux/src/components/Posts.js
@@ -7,6 +7,13 @@ class Posts extends Component {
     componentDidMount() {
         this.props.fetchPosts()
     }
+
+    componentWillReceiveProps(nextProps) {
+      if(nextProps.newPost) {
+        this.props.posts.unshift(nextProps.newPost)
+      }
+    }
+
   render() {
       const postItems = this.props.posts.map(post => {
           return (
@@ -26,10 +33,12 @@ class Posts extends Component {
 
 Posts.propTypes = {
     fetchPosts: PropTypes.func.isRequired,
-    posts: PropTypes.array.isRequired
+    posts: PropTypes.array.isRequired,
+    newPost: PropTypes.object
 }
 
 const mapStateToProps = state => ({
-    posts: state.posts.items
+    posts: state.posts.items,
+    newPost: state.posts.item
 })
 export default connect(mapStateToProps, { fetchPosts })(Posts)
